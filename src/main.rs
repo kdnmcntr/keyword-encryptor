8d17ada890650227dc570c6a8624ae526b98b863
Kaden McEntire <122327533+kdnmcntr@users.noreply.github.com>
2025-11-07 18:26:39 -0700

    Added timer and progress bar

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use std::env;
 use std::fs::{File};
 use std::thread;
 use std::sync::{Arc};
+use std::time::{Instant, Duration};
 use rand::{SeedableRng, RngCore};
 use rand_chacha::ChaCha20Rng;
 use io_at::{ReadAt, WriteAt};
+use indicatif::{ProgressBar, ProgressStyle};
 
 fn hash_string(in_string: &str) -> u64 {
 
@@ -22,7 +24,31 @@ fn hash_string(in_string: &str) -> u64 {
     hash_value 
 }
 
+fn format_time(time: Duration) -> String {
+    let mut secs = time.as_secs();
+    if secs < 1 {
+        return String::from(time.as_millis().to_string() + " milliseconds");
+    } else if secs < 60 {
+        return String::from(secs.to_string() + " seconds");
+    } else {
+        let mut mins = secs / 60;
+        secs = secs % 60;
+
+        if mins < 60 {
+            return String::from(mins.to_string() + " minutes, " + &secs.to_string() + " seconds");
+        } else {
+            let hours = mins / 60;
+            mins = mins % 60;
+            return String::from(hours.to_string() + " hours, " + &mins.to_string() + " minutes," + &secs.to_string() + " seconds");
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
+
+    //Start the timer
+    let timer = Instant::now();
+
     let args: Vec<String> = env::args().collect();
     if args.len() != 4 {
         println!("Incorrect number of arguments.");
@@ -36,8 +62,7 @@ fn main() -> io::Result<()> {
     let source_file = File::options().read(true).open(source)?;
     let output_file = File::create(output)?;
 
-    let source_metadata = source_file.metadata()?;
-    let file_size = source_metadata.len();
+    let file_size = source_file.metadata()?.len();
 
     output_file.set_len(file_size)?;
 
@@ -45,7 +70,7 @@ fn main() -> io::Result<()> {
     let mut rng = ChaCha20Rng::seed_from_u64(seed);
 
     let segment_size: u64 = 1048576; // 1 MB 1048576, 1 KB 1024
-    let segment_number: u64 = file_size / segment_size + 1;
+    let number_of_segments: u64 = file_size / segment_size + 1;
 
     let mut handles = vec![];
     let mut new_seeds = vec![]; 
@@ -53,12 +78,23 @@ fn main() -> io::Result<()> {
     let source_file = Arc::new(source_file);
     let output_file = Arc::new(output_file);
 
-    for i in 0..segment_number {
+    let bar = Arc::new(ProgressBar::new(file_size / 8 as u64));
+    bar.set_style(
+        ProgressStyle::with_template("[{bar:40.cyan/blue}] ({percent}%)")
+        .unwrap()
+        .progress_chars("#>-"),
+    );
+
+    println!("\nProcessing file...");
+
+    for i in 0..number_of_segments {
         new_seeds.push(rng.next_u64());
         let new_seeds_clone = new_seeds.clone();
 
         let source_file_clone = Arc::clone(&source_file);
         let output_file_clone = Arc::clone(&output_file);
+
+        let bar = Arc::clone(&bar);
 
         let handle = thread::spawn(move || {
             
@@ -79,7 +115,10 @@ fn main() -> io::Result<()> {
                 buffer = new_data.to_be_bytes(); // convert resulting u64 back into array of 8 bytes
                 output_file_clone.as_ref().write_at(&buffer, (segment_size * i) + (j * 8)).unwrap(); // write buffer to the output file
                 buffer = [0u8; 8]; // reset buffer
+
+                bar.inc(1);
             }
+
         });
 
         handles.push(handle);
@@ -88,6 +127,11 @@ fn main() -> io::Result<()> {
     for handle in handles {
         handle.join().unwrap();
     }
+
+    bar.finish();
+
+    let elapsed_time = timer.elapsed();
+    println!("Completed in {}.\n", format_time(elapsed_time));
 
     Ok(())
 
